33b3e7e2e86e35de4f3791527c26dfdf0a7fd0d0
Boswell Lin <38348033+buzzyballz@users.noreply.github.com>
2018-06-05 11:36:43 +1000

    Update game.py

--- scaffold/game.py
+++ scaffold/game.py
@@ -1,24 +1,42 @@
+import sys
+from loader import load_level
 
 def quit():
 	"""
 	Will quit the program
 	"""
-	pass
+	sys.exit()
 	
 def menu_help():
 	"""
 	Displays the help menu of the game
 	"""
-	pass
+	print("START <level file> - Starts the game with a provided file.")
+	print("QUIT - Quits the game")
+	print("HELP - Shows this message")
 
 def menu_start_game(filepath):
 	"""
 	Will start the game with the given file path
 	"""
-	pass
+	load_level(filepath)
 
 def menu():
 	"""
 	Start the menu component of the game
 	"""
-	pass
+	while True:
+		user_input = raw_input()
+		if 'START' in user_input:
+			print(user_input[6:])
+			menu_start_game(user_input[6:])
+		elif user_input == 'QUIT':
+			quit()
+		elif user_input == 'HELP':
+			menu_help()
+			continue
+		else:
+			print("No menu item")
+			continue
+
+menu()
